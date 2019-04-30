8e00b0d5a11cc2e0c92a9114bef40cb455a74612
unknown <mauro.hdz99@gmail.com>
2019-04-30 17:31:40 -0500

    Added switch cases statement

--- liri.js
+++ liri.js
@@ -11,3 +11,27 @@ require('dotenv').config()
 var isToday = require('date-fns/is_today')
 isToday(new Date())
 //=> true
+
+const search = process.argv[2];
+
+const topic = process.argv[3];
+
+//Tells the computer where to search based on what users is looking for
+switch (search) {
+    case "search-concerts":
+// Search the ticketmaster api
+
+    break;
+    case "search-songs":
+// Search the spotify api
+
+    break;
+    case "search-movie":
+// Search the OMDB api
+
+    break;
+    case "feeling-lucky":
+// Not sure yet
+
+    break;
+};
