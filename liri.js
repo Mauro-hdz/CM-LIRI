99c8cfa4ec660fc9796e718991755f1931ee8b3c
unknown <mauro.hdz99@gmail.com>
2019-04-30 03:02:32 -0500

    Added datefns and dotenv

--- liri.js
+++ liri.js
@@ -3,3 +3,11 @@ require("dotenv").config();
 const keys = require("./keys.js");
 
 const spotify = new Spotify(keys.spotify);
+
+// Used for dotenv
+require('dotenv').config()
+
+// Used for date-fns
+var isToday = require('date-fns/is_today')
+isToday(new Date())
+//=> true
